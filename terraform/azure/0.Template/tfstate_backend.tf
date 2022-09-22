8aed2ecfc4d3cfc633b32b08c8eb95d12ed3b8cf
Emmanuel Bergerat (MSFT) <emberger@microsoft.com>
2022-09-22 10:18:10 -0400

    minor case change

--- terraform/azure/0.Template/tfstate_backend.tf
+++ terraform/azure/0.Template/tfstate_backend.tf
@@ -13,7 +13,7 @@ terraform {
     resource_group_name  = "rg-ussc-azint-notepad-tfstates"
     storage_account_name = "stnotepadtfstates"
     container_name       = "github-embergershared-notepad"
-    key                  = # "azure_0-Template"
+    key                  = # "azure_0-template"
   }
 }
 #*/

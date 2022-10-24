3ba3ad0232ea5aa96316f47f32e3f201a59df6e9
Emmanuel <emberger@microsoft.com>
2022-10-24 12:15:30 -0400

    Adding ServiceBus client

--- azure/servicebus/securedsb/SecureSBClientUnitTests/MockServiceBusClient.cs
+++ azure/servicebus/securedsb/SecureSBClientUnitTests/MockServiceBusClient.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
 
 namespace ClientUnitTests
 {
-    internal class MockServiceBusClient
+    public internal class MockServiceBusClient : ServiceBusClient
     {
+        public override ServiceBusSender CreateSender(string queueOrTopicName, ServiceBusSenderOptions options)
+        {
+            return base.CreateSender(queueOrTopicName, options);
+        }
+
+        public override ValueTask DisposeAsync()
+        {
+            return base.DisposeAsync();
+        }
     }
 }

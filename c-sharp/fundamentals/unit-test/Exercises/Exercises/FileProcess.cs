161a48bc1da09fb141f393b48c71eb650883d5a6
Emmanuel <emberger@microsoft.com>
2022-11-09 23:59:51 -0500

    Init exercise

---
+++ c-sharp/fundamentals/unit-test/Exercises/Exercises/FileProcess.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercises
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            return File.Exists(fileName);
+        }
+    }
+}

9221b1afc2c8045064fd528e28372f8908dca333
Emmanuel <emberger@microsoft.com>
2022-11-10 00:32:46 -0500

    Clean starting state for unit tests

--- c-sharp/fundamentals/unit-test/Exercises/ClassesLibrary/FileProcess.cs
+++ c-sharp/fundamentals/unit-test/Exercises/ClassesLibrary/FileProcess.cs
@@ -1,4 +1,4 @@
-﻿namespace Exercises
+﻿namespace ClassesLibrary
 {
     public class FileProcess
     {
---
+++ c-sharp/fundamentals/unit-test/Exercises/ClassesTests/FileProcessShould.cs
@@ -0,0 +1,50 @@
+namespace ClassesTests
+{
+    [TestClass]
+    public class FileProcessShould
+    {
+        [TestMethod]
+        public void FileExists_FileExists_ReturnsBool()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileExists_FileDoesNotExist_ReturnsBool()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileExists_FileNameIsNullOrEmptyUsingAttribute_ThrowException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileExists_FileNameIsNullOrEmptyUsingTryCatch_ThrowException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Inconclusive();
+        }
+    }
+}

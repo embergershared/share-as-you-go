3e13ff5c003a60cc19fd083e1353dab96cf33b51
Emmanuel <emberger@microsoft.com>
2022-11-12 00:16:01 -0500

    Various mock setup to control return values

--- c-sharp/fundamentals/unit-test/xUnit/Mocking/ClassLibrary/CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
+++ c-sharp/fundamentals/unit-test/xUnit/Mocking/ClassLibrary/CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using Moq;
 
 namespace ClassLibrary.Tests
 {
@@ -8,7 +9,8 @@ namespace ClassLibrary.Tests
         public void AutoAccept_HighIncomeApplications()
         {
             // Arrange
-            var sut = new CreditCardApplicationEvaluator(null);
+            Mock<IFrequentFlyerValidator> mockFfValidator = new Mock<IFrequentFlyerValidator>();
+            var sut = new CreditCardApplicationEvaluator(mockFfValidator.Object);
             var application = new CreditCardApplication { GrossAnnualIncome = 110_000 };
             var expected = CreditCardApplicationDecision.AutoAccepted;
 
@@ -23,9 +25,51 @@ namespace ClassLibrary.Tests
         public void Refer_YoungApplications()
         {
             // Arrange
-            var sut = new CreditCardApplicationEvaluator(null);
+            var mockFfValidator = new Mock<IFrequentFlyerValidator>();
+            var sut = new CreditCardApplicationEvaluator(mockFfValidator.Object);
             var application = new CreditCardApplication { Age = 19 };
             var expected = CreditCardApplicationDecision.ReferredToHuman;
+
+            // Act
+            var actual = sut.Evaluate(application);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AutoDecline_LowIncomeApplications()
+        {
+            // Arrange
+            var mockFfValidator = new Mock<IFrequentFlyerValidator>();
+            // mockFfValidator.Setup(m => m.IsValid("x")).Returns(true);
+            //mockFfValidator.Setup(
+            //        m => m.IsValid(It.IsAny<string>()))
+            //        .Returns(true);
+            //mockFfValidator.Setup(
+            //        m => m.IsValid(
+            //            It.Is<string>(text => text.StartsWith("t"))))
+            //    .Returns(true);
+            //mockFfValidator.Setup(
+            //        m => m.IsValid(
+            //            It.IsInRange<string>("a", "h", Range.Inclusive)))
+            //    .Returns(true);
+            //mockFfValidator.Setup(
+            //        m => m.IsValid(
+            //            It.IsIn<string>("a", "x", "test")))
+            //    .Returns(true);
+            mockFfValidator.Setup(
+                    m => m.IsValid(
+                        It.IsRegex("[a-z]")))
+                .Returns(true);
+
+
+            var sut = new CreditCardApplicationEvaluator(mockFfValidator.Object);
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 19_999, Age = 42, FrequentFlyerNumber = "y"
+            };
+            var expected = CreditCardApplicationDecision.AutoDeclined;
 
             // Act
             var actual = sut.Evaluate(application);

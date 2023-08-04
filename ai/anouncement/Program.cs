144fcf7c3c8138c4c90a197ca095e5e74aa43777
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-08-04 13:02:37 -0400

    AI speech to translated text quickstart

---
+++ ai/anouncement/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+
+class Program
+{
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    private static readonly string SpeechKey = Environment.GetEnvironmentVariable("SPEECH_KEY") ?? throw new InvalidOperationException();
+    private static readonly string SpeechRegion = Environment.GetEnvironmentVariable("SPEECH_REGION") ?? throw new InvalidOperationException();
+
+    static void OutputSpeechRecognitionResult(TranslationRecognitionResult translationRecognitionResult)
+    {
+        switch (translationRecognitionResult.Reason)
+        {
+            case ResultReason.TranslatedSpeech:
+                Console.WriteLine($"RECOGNIZED: Text={translationRecognitionResult.Text}");
+                foreach (var element in translationRecognitionResult.Translations)
+                {
+                    Console.WriteLine($"TRANSLATED into '{element.Key}': {element.Value}");
+                }
+                break;
+            case ResultReason.NoMatch:
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = CancellationDetails.FromResult(translationRecognitionResult);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+    }
+
+    async static Task Main(string[] args)
+    {
+        var speechTranslationConfig = SpeechTranslationConfig.FromSubscription(SpeechKey, SpeechRegion);
+        speechTranslationConfig.SpeechRecognitionLanguage = "en-US";
+        speechTranslationConfig.AddTargetLanguage("es");
+
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var translationRecognizer = new TranslationRecognizer(speechTranslationConfig, audioConfig);
+
+        Console.WriteLine("Speak into your microphone.");
+        var translationRecognitionResult = await translationRecognizer.RecognizeOnceAsync();
+        OutputSpeechRecognitionResult(translationRecognitionResult);
+    }
+}

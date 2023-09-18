6d579ee579ffe738428cfc9f216f7dae18f74571
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-09-18 01:54:18 -0400

    switched WebClient for HttpClient

--- azure/secureAppService/FunctionApp1/WhatIsMyIP.cs
+++ azure/secureAppService/FunctionApp1/WhatIsMyIP.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -9,15 +11,49 @@ namespace FunctionApp1
     public class WhatIsMyIP
     {
         [FunctionName("WhatIsMyIP")]
-        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("*/10 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
             log.LogInformation("Querying the Public IP");
-            var client = new WebClient();
-            var content = client.DownloadString("http://icanhazip.com/");
 
-            log.LogInformation($"The Public IP of this function app is: {content}");
+            log.LogInformation("   Using HttpClient");
+
+            HttpClient httpClient = new()
+            {
+                BaseAddress = new Uri("http://icanhazip.com/"),
+            };
+
+            using var response = await httpClient.GetAsync("/");
+
+            response.EnsureSuccessStatusCode();
+                //.WriteRequestToConsole();
+            var body = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"[Console] The Public IP seen is: {body}");
+            log.LogInformation($"[Log] The Public IP seen is: {body}");
+
+            //log.LogInformation("   Using WebClient");
+            //var client = new WebClient();
+            //var content = client.DownloadString("http://icanhazip.com/");
+
+            //log.LogInformation($"The Public IP of this function app is: {content}");
+        }
+    }
+
+    static class HttpResponseMessageExtensions
+    {
+        internal static void WriteRequestToConsole(this HttpResponseMessage response)
+        {
+            if (response is null)
+            {
+                return;
+            }
+
+            var request = response.RequestMessage;
+            Console.Write($"{request?.Method} ");
+            Console.Write($"{request?.RequestUri} ");
+            Console.WriteLine($"HTTP/{request?.Version}");
         }
     }
 }
